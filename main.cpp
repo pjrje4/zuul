c92c763ab56a57b3d374285f4a43de1bd9e32c8e
pjrje4 <58542025+pjrje4@users.noreply.github.com>
2021-12-13 13:32:19 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,39 +9,56 @@ Main for Zuul project
 
 using namespace std;
 
-void go(int dir);
-void pick();
-void drop();
+void go(int dir, vector<Room*>&);
+void pick(vector<Room*>&);
+void drop(vector<Room*>&);
 
 int main() {
+	vector<Room*> rooms;
 	while (true) {
 		char input[20];
 		cout << "Enter a command (N, W, E, S, PICK, DROP, QUIT): ";
 		cin.getline(input, 19);
 		if (strcmp(input, "N") == 0) {
-			go(1);
+			go(1, rooms);
 		}
 		else if (strcmp(input, "W") == 0) {
-                        go(2);
+                        go(2, rooms);
                 }
 		else if (strcmp(input, "E") == 0) {
-                        go(3);
+                        go(3, rooms);
                 }
 		else if (strcmp(input, "S") == 0) {
-                        go(4);
+                        go(4, rooms);
                 }
 		else if (strcmp(input, "PICK") == 0) {
-                        pick();
+                        pick(rooms);
                 }
 		else if (strcmp(input, "DROP") == 0) {
-                        drop();
+                        drop(rooms);
                 }
-		else if (strcmp(input, "PICK") == 0) {
-                        pick();
+		else if (strcmp(input, "QUIT") == 0) {
+                        return 0;
                 }
+	}
+}
+void go(int dir, vector<Room*>&) {
+	if (dir == 1) {
+		//north
+	}
+	if (dir == 2) {
+		//west
+	}
+	if (dir == 3) {
+		//east
+	}
+	if (dir == 4) {
+		//south
+	}
+}
+void pick(vector<Room*>&) {
 
+}
+void drop(vector<Room*>&) {
 
-
-
-
-			
+}
